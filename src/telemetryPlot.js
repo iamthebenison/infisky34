cba20f301235a694a9133afaa5e02e039d806781
Vara <saisrinivasvaraprasadk@gmail.com>
2024-03-14 22:40:44 +0530

    Tried with less luck

--- src/telemetryPlot.js
+++ src/telemetryPlot.js
@@ -1,4 +1,4 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 import { Mission1 } from './components';
 import { initialState, parseJSON } from './lib';
 
@@ -7,39 +7,53 @@ class Telemetry extends Component {
     super(props);
 
     this.state = initialState();
-    console.log("Hello!")
-    setInterval(() => {
-      fetch('http://localhost:3000/TelemetryData.json',{cache: "reload"})
-      .then(response => {
-         try{
-           return response.json();
-         }catch (e) {
-          return Promise.reject();
-        }
-      })
-      .then( telemetryData => {
-         this.setState(parseJSON(this.state,telemetryData));
-      })
-      .catch(() => {});
-    }, 1000);
+    this.timerId = null;
   }
 
-  changePlots(state,plots){
-    this.setState(
-      Object.freeze({
-      ...state,
-      plotsToRender : plots
-    })
-  );
-  }
+  startMission = () => {
+    // Stop any ongoing mission first
+    this.stopMission();
+
+    let currentIndex = 0;
+    const data = [];
+
+    // Fetch data from TelemetryData.json periodically
+    this.timerId = setInterval(() => {
+      fetch('http://localhost:3000/TelemetryData.json', { cache: "reload" })
+        .then(response => response.json())
+        .then(telemetryData => {
+          if (currentIndex < telemetryData.length) {
+            // Parse and update the state with the current data point
+            const parsedData = parseJSON(this.state, telemetryData[currentIndex]);
+            this.setState(parsedData);
+            currentIndex++;
+          } else {
+            // If all data points have been fetched, stop the mission
+            this.stopMission();
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching telemetry data:', error);
+          // If an error occurs, stop the mission
+          this.stopMission();
+        });
+    }, 2000); // Fetch data every 2 seconds
+};
+
+
+  stopMission = () => {
+    // Stop the mission by clearing the interval
+    clearInterval(this.timerId);
+  };
+
   render() {
     return (
       <div id="plots">
-          <Mission1 state={this.state}/>
+        <button onClick={this.startMission}>Start Mission</button>
+        <Mission1 state={this.state} />
       </div>
     );
   }
-
 }
 
 export default Telemetry;
